464772a3bafc3afdfbb92cf9aee6fcfb4b279532
rherzerusc <rherzer@email.sc.edu>
2024-06-09 15:14:16 -0400

    Update jsproject.js

--- jsproject.js
+++ jsproject.js
@@ -1,17 +1,15 @@
 document.addEventListener("DOMContentLoaded", function () {
     // Social Media Sharing Functions
     function shareOnFacebook() {
-    // Replace 'your-website-url' with the URL you want to share
-    const urlToShare = 'https://www.example.com';
+    const urlToShare = 'https://rherzerusc.github.io/itec362-website/wip.html';
     const facebookShareUrl = `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(urlToShare)}`;
     window.open(facebookShareUrl, 'Share on Facebook', 'width=600,height=400');
     }
     function shareOnTwitter() {
-    // Replace 'your-website-url' and 'your-text' with your desired URL and text
-    const urlToShare = 'your-website-url';
-    const textToShare = 'your-text';
+    const urlToShare = 'https://rherzerusc.github.io/itec362-website/wip.html';
+    const textToShare = "Check out Renee Herzer's WIP!";
     const twitterShareUrl = `https://twitter.com/intent/tweet?url=${encodeURIComponent(urlToShare)}&text=${encodeURIComponent(textToShare)}`;
-    window.open(twitterShareUrl, 'Share on Twitter', 'width=600,height=400');
+    window.open(twitterShareUrl, 'Share on X', 'width=600,height=400');
     }
     // Event listeners for social media sharing buttons
     const shareFacebookButton = document.getElementById("share-facebook");
